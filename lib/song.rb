f48321a77f4725f36af9656f15e302ca563bc565
Greg Morgan <gregm9999@gmail.com>
2018-11-14 00:46:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
   include Paramable::InstanceMethods
   @@songs = []
 
-  # def initialize
-  #   self.class.all << self
-  # end
+  def initialize
+    self.class.all << self
+  end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
